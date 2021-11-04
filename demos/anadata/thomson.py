6de199933b9e6f9ee3e12aa1a3f7d5a40eb6bdde
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2021-11-04 17:40:45 +0900

    [update] modify AnaData class
    enable AnaData to handle retireved data as xarray Dataset.

--- demos/anadata/thomson.py
+++ demos/anadata/thomson.py
@@ -1,38 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from nifs.anadata import AnaData
 import matplotlib.pyplot as plt
 
 
-# トムソン のショット番号 8000, サブショット番号 1 のデータを取得
-data = AnaData.retrieve("thomson", 80000, 1)
+# instatiate AnaData class at shotNo: 80000, sub-shotNo: 1
+ana = AnaData(80000, 1)
 
-# 次元 'R' のデータを取得 (要素数137 の一次元配列が返ってくる)
-r = data.getDimData("R")
-r_unit = data.getDimUnit("R")
+# retrieve datset of "thomson" diagnostics
+ds = ana.retrieve("thomson")
 
+# shot dataset information
+print(ds)
 
-# 次元 'Time' のデータを取得 (要素数96 の一次元配列が返ってくる)
-t = data.getDimData("Time")
+# plot Te as function of R varied between 1000 to 1100 [ms].
+fig1, ax1 = plt.subplots()
+ds.Te.sel(Time=slice(1000, 1100)).plot.line(x="R", ax=ax1)
+ax1.set_title("Observed Te by thomson scattering meseurement")
 
-# 変数 'Te' のデータを取得 (要素数 136x96 の二次元配列が返る)
-Te = data.getValData("Te")
+# plot Te in 2-dimension (R - Time)
+fig2, ax2 = plt.subplots()
+ds.Te.plot(x="Time", y="R", ax=ax2)
+ax2.set_title("Observed Te by thomson scattering meseurement")
 
-# 各時刻毎の R 対 Te のデータをプロット
-
-plt.title("Thomson")
-plt.xlabel("R [" + r_unit + "]")
-plt.ylabel("Te")
-labels = []
-
-# 5 データ取得
-for i in range(5):
-    T = Te[i, :]  # R 次元でのスライスを取得
-    plt.plot(r, T)
-    label = "t = %05.3f (sec)" % (t[i] / 1000.0)  # 凡例用文字列
-    labels.append(label)
-
-plt.legend(labels)  # 凡例セット
-
-plt.show()  # グラフ描画
+plt.show()
--- nifs/anadata/anadata.py
+++ nifs/anadata/anadata.py
@@ -1,99 +1,151 @@
 # -*- coding: utf-8 -*-
-import re
+import traceback
+import xarray as xr
 from ._anadata import _AnaData
+from nifs.database import get_connection
 
 VER = "$Id: anadata.py,v 1.2 2020/07/15 02:59:48 yoshida Exp $"
 
 
-class _AnaDataInfo:
-
-    def __init__(self, ary):
-        (
-            self.diagnostics,
-            self.server,
-            self.directory,
-            self.filename,
-            self.userid,
-            self.summary,
-            self.shotno,
-            self.subshotno,
-            self.filelen,
-        ) = ary
-
-
-class AnaData(_AnaData):
+class AnaData:
     """
     AnaData
     =======
 
-    class to hold analysis data.
-    Main methods are implemeted by the base class _AnaData.
+    class to handle analyzed data stored in analysis (kaiseki) server.
+    When retrieving or registering data to kaiseki server,
+    wrapper class :py:class:_AnaData which is implemeted in libana2 is used.
+    Retrieved data is reconstructed as one dataset
+    using `xarray package <http://xarray.pydata.org/>`_
 
-    Attributes
-    -----------
-    retrieve
+    Parameters
+    ----------
+    shot : int, required
+        shot number
+    subshot : int, optional
+        sub-shot number, by default 1
+
+    Example
+    --------
+    .. prompt:: python >>> auto
+        >>> from nifs.anadata import AnaData
+        >>>
+        >>> ana = AnaData(80000, 1)
     """
 
-    def __new__(cls, *av):
-        return super(AnaData, cls).__new__(cls, *av)
+    def __init__(self, shot: int, subshot=1) -> None:
+        # set initial values to prevent undefined behaviour.
+        self._shot = shot
+        self._subshot = subshot
+        self._diagnostics = None
+        # open database
+        self.database = get_connection()
 
-    #    def __init__(self, *av):
-    #        print "AnaData.__init__"
-    #        _AnaData.__init__(self,*av)
+    def __del__(self) -> None:
+        # close database
+        self.database.close()
 
-    def getUserDict(self):
-        """
-        convert the comments to dict type valueables.
-        """
-        comment = self.getComment()
-        rgx = re.compile("^\s*(.*)=(.*)\s*$")
-        ret = {}
-        for line in comment.splitlines():
-            m = rgx.match(line)
-            if m:
-                key = m.group(1).strip().upper()
-                val = m.group(2).strip()
-                ret[key] = val
-        return ret
-
-    @classmethod
-    def search(cls, diagnostics, upper=1, lower=0, subno=1, userid=None, limit=100):
-        """Search for registered data that maches the arguments.
-
-        Parameters
-        ----------
-        diagnostics : list
-            list of diagnostics name.
-        upper : int
-            upper shot number, by default 1
-        lower : int
-            lower shot number, by default 0
-        subno : int, optional
-            sub-shotnumber, by default 1
-        userid : int, optional
-            ID number of the registrant, by default None
-        limit : int, optional
-            maximum nuber of searches, by default 100
+    @property
+    def shot(self) -> int:
+        """Shot number.
 
         Returns
         -------
-        list
-            containing _AnaDataInfo instances which have shot information.
-
-        Example
-        --------
-        .. prompt:: python >>> auto
-
-            >>> from nifs.anadata import Anadata
-            >>> AnaData.search("Bolometer", upper=10010, lower=10000)
-            [_AnaDataInfo]
+        int
         """
-        r = _AnaData.search(diagnostics, upper, lower, subno, userid, limit)
-        return [_AnaDataInfo(e) for e in r]
+        return self._shot
 
+    def retrieve(self, diagnostics: str) -> xr.Dataset:
+        """Retrieving analyzed data from kaiseki-server.
+        This methods returns a xarray.Dataset object.
+        If you want to know about xarray, please see the xarray HP:
+        http://xarray.pydata.org/.
 
-#    @classmethod
-#    def retrieve(self, *av):
-#        ana = _AnaData.retrieve(*av)
-#        ana.__class__ = AnaData
-#        return ana
+        Parameters
+        ----------
+        diagnostics : str
+            name of diagnostics which is registred in the kaiseki-server.
+
+        Returns
+        -------
+        xarray.Dataset
+            dataset of a xarray object.
+
+        Examples
+        --------
+        .. prompt :: python >>> auto
+            >>> from nifs.anadata import AnaData
+            >>>
+            >>> ana = AnaData(80000, 1)
+            >>> # Retrieveing dataset of thomson
+            >>> ds = ana.retrieve("thomson")
+            >>> ds
+            <xarray.Dataset>
+            Dimensions:       (Time: 96, R: 137)
+            Coordinates:
+            * Time          (Time) float64 0.0 33.0 66.0 ... 3.1e+03 3.133e+03 3.166e+03
+            * R             (R) float64 2.525e+03 2.55e+03 ... 4.884e+03 4.895e+03
+            Data variables:
+                Te            (Time, R) float64 84.0 0.0 38.0 47.0 ... 5.0 73.0 5.0 125.0
+                dTe           (Time, R) float64 2.178e+04 0.0 40.0 ... 2.178e+04 2.178e+04
+                n_e           (Time, R) float64 9.0 138.0 16.0 4.0 ... 38.0 11.0 0.0 16.0
+                dn_e          (Time, R) float64 8.0 63.0 7.0 6.0 7.0 ... 13.0 8.0 11.0 7.0
+                laser         (Time, R) float64 5.209e+03 5.209e+03 ... 4.373e+03 4.373e+03
+                laser number  (Time, R) float64 5.0 5.0 5.0 5.0 5.0 ... 1.0 1.0 1.0 1.0 1.0
+            Attributes:
+                diagnostics:  thomson
+                description:  density (ne) is only for very rough information\nDo not use...
+        """
+        ana = _AnaData.retrieve(diagnostics, self.shot, self._subshot)
+
+        dim_num = ana.getDimNo()
+        val_num = ana.getValNo()
+
+        coords = {ana.getDimName(i): ana.getDimData(i) for i in range(dim_num)}
+        data_vars = {
+            ana.getValName(i): ([key for key in coords.keys()], ana.getValData(i), {"units": ana.getValUnit(i)})
+            for i in range(val_num)
+        }
+        attrs = {"diagnostics": diagnostics, "description": ana.getComment()}
+
+        ds = xr.Dataset(data_vars=data_vars, coords=coords, attrs=attrs)
+        for i in range(dim_num):
+            ds[ana.getDimName(i)].attrs["units"] = ana.getDimUnit(i)
+
+        return ds
+
+    def diagnostics_list(self) -> dict:
+        """Registered diagnostics names at a shot number.
+
+        Returns
+        -------
+        dict
+            {name: comment}
+
+        Examples
+        --------
+        .. prompt :: python >>> auto
+            >>> from nifs.anadata import AnaData
+            >>> ana = AnaData(80000, 1)
+            >>> ana.diagnostics_list()
+            {'ha1': 'Time behaviors of visible lines of Halpha and HeI',
+             'ech': 'Wave form of ECH pulse',
+             'nb1pwr': 'NBI power of BL1',
+             'imp01': 'Line intensities of Lyman-alpha and some impurity ions',
+             'wp': 'Plasma stored energy estimated by diamagnetic flux measurement',...
+            }
+        """
+        try:
+            cursor = self.database.cursor()
+            sql = (
+                "SELECT d.name, d.diagcomment "
+                "FROM diagnostics AS d "
+                "JOIN datalocation AS dl ON dl.did = d.id "
+                f"WHERE dl.shotno = {self.shot}"
+            )
+            cursor.execute(sql)
+            diag_list = cursor.fetchall()
+            return {name: comment for name, comment in diag_list}
+
+        except Exception:
+            traceback.print_exc()
